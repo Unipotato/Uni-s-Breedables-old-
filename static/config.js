ae075357bd6e1cd119f25c1ba7aeca66e97e930e
Unipotato <dzakmal@gmail.com>
2017-01-03 13:20:06 +0800

    update config.js

--- static/config.js
+++ static/config.js
@@ -21,10 +21,10 @@ var inGameName = "Uni";
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/UniLune";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/c472dec0-b473-11e6-8169-06598ddd6773.png";
